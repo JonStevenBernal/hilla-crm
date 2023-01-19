7402b82afb1abf61fea42233a09fdfaefd15a056
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2023-01-19 18:51:18 -0500

    Tutorial hilla

--- src/main/java/com/example/application/data/repository/ContactRepository.java
+++ src/main/java/com/example/application/data/repository/ContactRepository.java
@@ -2,7 +2,8 @@ package com.example.application.data.repository;
 
 import com.example.application.data.entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
-public interface ContactRepository extends JpaRepository<Contact, Long> {
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
 
 }
